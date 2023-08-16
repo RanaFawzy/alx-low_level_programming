11d8ca0bfc7ea9527c43da7a488e682a21c28b68
RanaFawzy <rororona91@gmail.com>
2023-08-15 20:28:03 -0700

    add 5 file

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 int r;
@@ -20,8 +21,7 @@ _putchar(r + '0');
 _putchar('\n');
 r = print_sign(0xff);
 _putchar(',');
-_putchar(' ');
-_putchar(r + '0');
+_putchar(' ');_putchar(r + '0');
 _putchar('\n');
 r = print_sign(-1);
 _putchar(',');
