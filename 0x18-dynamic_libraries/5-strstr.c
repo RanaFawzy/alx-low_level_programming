c46a17b0f9603b5b7a7bf1ed541cf5b32e4f0aae
RanaFawzy <rororona91@gmail.com>
2023-11-16 14:17:32 -0800

    Test

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry pointtt
+ * @haystack: inputtt
+ * @needle: inputtt
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *l = haystack;
+char *p = needle;
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
+}
